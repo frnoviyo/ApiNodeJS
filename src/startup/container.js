da12ae0d9bf1f2fe2212d34e4516ebaab1360dc0
Freddy Novillo <freddy.novillo@bayteq.com>
2021-03-22 22:33:41 -0500

    [ADD] firts steps added

---
+++ src/startup/container.js
@@ -0,0 +1,33 @@
+const { createContainer, asClass, asFunction, asValue } = require('awilix');
+
+//consfig
+const config = require('../config');
+const app = require('.') //llama al archivo index.js
+
+//Services
+const { HomeService } = require('../services');
+
+//Controllers
+const { HomeController } = require('../controllers');
+
+//Routes
+const { HomeRoutes } = require('../routes/index.routes')
+const Routes = require('../routes');
+
+const container = createContainer();
+
+container
+  .register({
+    app: asClass(app).singleton(),
+    router: asFunction(Routes).singleton(),
+    config: asValue(config)
+  })
+  .register({
+    HomeService: asClass(HomeService).singleton()
+  }).register({
+    HomeController: asClass(HomeController.bind(HomeController)).singleton()
+  }).register({
+    HomeRoutes: asFunction(HomeRoutes).singleton()
+  });
+
+module.exports = container;
