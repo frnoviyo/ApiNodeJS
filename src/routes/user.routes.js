caad0815d32677decbaa0a8e03699dcc44836b00
Freddy Novillo <freddy.novillo@bayteq.com>
2021-04-06 20:10:10 -0500

    [ADD] caching implement

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
-const { AuthMiddleware, ParseIntMiddleware } = require('../middlewares');
+const { AuthMiddleware, ParseIntMiddleware, CacheMiddelware } = require('../middlewares');
+const { CACHE_TIME } = require('../helpers');
 
 module.exports = function ({ UserController }) {
   const router = Router();
 
   router.get('/:userId', UserController.get);
-  router.get('', [AuthMiddleware, ParseIntMiddleware],UserController.getAll);
+  router.get('', [AuthMiddleware, ParseIntMiddleware, CacheMiddelware(CACHE_TIME.ONE_HOUR)],UserController.getAll);
   router.patch('/:userId', UserController.update);
   router.delete('/:userId', UserController.delete);
 
