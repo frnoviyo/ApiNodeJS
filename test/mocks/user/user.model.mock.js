624998c72fc0c4a174f9819e573a66a4f8d03d0c
Freddy Novillo <frnoviyo@gmail.com>
2021-04-10 17:16:43 -0500

    [ADD] test added

---
+++ test/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+  user: {
+    _id: '507f191e810c19729de860ea',
+    name: 'Freddy',
+    username: 'frnoviyo',
+    password: '123456'
+  },
+  users: [
+    {
+      _id: '507f191e810c19729de860ec',
+      name: 'Joaquin',
+      username: 'joaco',
+      password: '123456'
+    },
+    {
+      _id: '507f191e810c19729de860eb',
+      name: 'Jose',
+      username: 'puma',
+      password: '123456'
+    }
+  ]
+}
